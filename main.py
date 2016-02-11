13dfbc0e27ac61fc8f50adf9c121edd2f79f104b
Charles Pittman <charles.pittman@gmail.com>
2016-02-10 19:12:38 -0500

    Adding main file skeleton

---
+++ main.py
@@ -0,0 +1,33 @@
+def initialize():
+    """Sets everything up.  Not sure what goes here."""
+    pass
+
+
+def flag_up():
+    """Returns True if flag is up."""
+    pass
+
+
+def door_open():
+    """Returns True if door is open."""
+    pass
+
+
+def mail_here():
+    """Returns True if there's mail in the box."""
+    pass
+
+
+def lock_door():
+    """Locks door.."""
+    pass
+
+
+def unlock_door():
+    """Unlocks door.."""
+    pass
+
+
+def send_alert():
+    """Sends a text message with picture attached."""
+    pass
