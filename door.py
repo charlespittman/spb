67bde1ac6fd10955384d047add63b2f372ebad12
Charles Pittman <charles.pittman@gmail.com>
2016-02-17 13:34:14 -0500

    Collapse door stuff.
    - Combined lock/unlock
    - Updated README

---
+++ door.py
@@ -0,0 +1,20 @@
+# Locks or unlocks door.
+
+import RPi.GPIO as GPIO
+
+# Set pin numbering to Broadcom type
+GPIO.setmode(GPIO.BOARD)
+
+LOCK = 7  # Connected to Relay K2
+GPIO.setup(LOCK, GPIO.OUT)
+
+UNLOCK = 8  # Connected to Relay K1
+GPIO.setup(UNLOCK, GPIO.OUT)
+
+
+def lock_door():
+    GPIO.output(LOCK, True)
+
+
+def unlock_door():
+    GPIO.output(UNLOCK, True)
