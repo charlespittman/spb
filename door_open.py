e485bb2b8b5bf888a40c1b0dcd9d0ed4283bb3c2
Charles Pittman <charles.pittman@gmail.com>
2016-02-17 13:16:31 -0500

    Fixup door_open.py
    - Removed extraneous comments
    - Removed unused sys import
    - Removed pointless sleep
    - Fixed syntax error(s)

--- door_open.py
+++ door_open.py
@@ -1,11 +1,5 @@
-#This subroutine will poll pin 37 to determine the state of the door
-#is it  OPEN    = True
-# or    Closed  = False
-#GPIO 37 = Door Close
-#GPIO 16 = Return Statment
+# This subroutine will poll pin 37 to determine the state of the door.
 
-
-import sys
 import RPi.GPIO as GPIO
 
 # Set pin numbering to Broadcom type
@@ -14,19 +8,13 @@ GPIO.setmode(GPIO.BOARD)
 DOORCLOSE = 37
 DOORSTATUS = 16
 
-# Set pin 37 to input
-GPIO.setup(DOORCLOSE, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+GPIO.setup(DOORCLOSE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# try/finally tries to run everything in the "try" block, and ensures "finally"
-# runs either way.  Used here to cleanup the GPIO pins.
 try:
     if (not GPIO.input(DOORCLOSE)):
-            GPIO.output(DOORSTATUS, True)           # Set pin high
-            else (GPIO.output(DOORSTATUS,False))    # Set pin low
-        time.sleep(0.1)
-    
-finally:
-    GPIO.cleanup()           #Sets only pins used in this program back to input
+        GPIO.output(DOORSTATUS, True)
+    else:
+        GPIO.output(DOORSTATUS, False)
 
-# Clean exit
-sys.exit(0)
+finally:
+    GPIO.cleanup()  # Sets only pins used in this program back to input
