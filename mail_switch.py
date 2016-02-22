b84854950a6083305b695d1119b1b4eca98f121c
Charles Pittman <charles.pittman@gmail.com>
2016-02-22 18:40:42 -0500

    Fixup mail_switch.
    - Removed some extra comments.
    - Added a main section.
    - Added is_mail().
    - Removed extra pin.

--- mail_switch.py
+++ mail_switch.py
@@ -1,32 +1,38 @@
-#This subroutine will poll pin 36 to determine if is was pressed
-#GPIO 36 = mail trigger
-#GPIO 32 = yes mail
+# This subroutine will poll pin 36 to determine if is was pressed
 
-
-import sys
+import time
 import RPi.GPIO as GPIO
 
 # Set pin numbering to Broadcom type
 GPIO.setmode(GPIO.BCM)
 
 MAILSWITCH = 36
-YESMAIL = 32
-
-# Set pin 36 to input
 GPIO.setup(MAILSWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# try/finally tries to run everything in the "try" block, and ensures "finally"
-# runs either way.  Used here to cleanup the GPIO pins.
 
-try:
-    while(True):
-        if (not GPIO.input(MAILSWITCH)):
-            GPIO.output(YESMAIL, True)  # Set pin high
-            send_mail()                 # call subroutine to send SMS
-        time.sleep(0.1)
-    
-finally:
-    GPIO.cleanup()           #Sets only pins used in this program back to input
+def is_mail():
+    """Return True if MAILSWITCH is triggered."""
+    if not GPIO.input(MAILSWITCH):
+        return True
+    else:
+        return False
 
-# Clean exit
-sys.exit(0)
+
+def send_mail():
+    """Send mail message."""
+    pass
+
+
+def main():
+    """Poll for mail trigger."""
+    try:
+        while(True):
+            if is_mail():
+                send_mail()
+                time.sleep(0.1)
+
+    finally:
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
