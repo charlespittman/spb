51734e4217be641aac324f7191c25703539a3916
Charles Pittman <charles.pittman@gmail.com>
2016-02-24 20:25:09 -0500

    Adding door switch.
    - Added two functions to read reed switch.
    - Updated README with correct pin.

--- door.py
+++ door.py
@@ -12,17 +12,38 @@ GPIO.setup(LOCK, GPIO.OUT, initial=GPIO.HIGH)
 UNLOCK = 21  # Connected to Relay K1
 GPIO.setup(UNLOCK, GPIO.OUT, initial=GPIO.HIGH)
 
+door_switch = 12
+GPIO.setup(door_switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 def lock_door():
     """Locks door on Relay K2"""
     GPIO.output(LOCK, GPIO.LOW)
+    time.sleep(1)
     GPIO.output(LOCK, GPIO.HIGH)
 
 
 def unlock_door():
     """Unlocks door on Relay K1."""
     GPIO.output(UNLOCK, GPIO.LOW)
+    time.sleep(1)
     GPIO.output(UNLOCK, GPIO.HIGH)
+
+
+def is_closed():
+    """Return True if door is closed."""
+    if GPIO.input(door_switch):
+        return True
+    else:
+        return False
+
+
+def is_open():
+    """Return True if door is open."""
+    if is_closed():
+        return False
+    else:
+        return True
 
 
 def main():
