3b3b7e01e264a947c60c4534dbba64f28f0929cd
Charles Pittman <charles.pittman@gmail.com>
2016-03-02 20:21:45 -0500

    Adding Door class

--- door.py
+++ door.py
@@ -6,52 +6,55 @@ import RPi.GPIO as GPIO
 # Set pin numbering to Broadcom type
 GPIO.setmode(GPIO.BCM)
 
-LOCK = 20  # Connected to Relay K2
-GPIO.setup(LOCK, GPIO.OUT, initial=GPIO.HIGH)
 
-UNLOCK = 21  # Connected to Relay K1
-GPIO.setup(UNLOCK, GPIO.OUT, initial=GPIO.HIGH)
+class Door(object):
+    """Class to encapsulate the GSM module."""
 
-door_switch = 12
-GPIO.setup(door_switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    def __init__(self, lock_pin, unlock_pin, switch_pin):
+        self.lock_pin = lock_pin
+        GPIO.setup(self.lock_pin, GPIO.OUT, initial=GPIO.HIGH)
 
+        self.unlock_pin = unlock_pin
+        GPIO.setup(self.unlock_pin, GPIO.OUT, initial=GPIO.HIGH)
 
-def lock_door():
-    """Locks door on Relay K2"""
-    GPIO.output(LOCK, GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(LOCK, GPIO.HIGH)
+        self.switch_pin = switch_pin
+        GPIO.setup(self.switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    def lock(self):
+        """Locks door on Relay K2"""
+        GPIO.output(self.lock_pin, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(self.lock_pin, GPIO.HIGH)
 
-def unlock_door():
-    """Unlocks door on Relay K1."""
-    GPIO.output(UNLOCK, GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(UNLOCK, GPIO.HIGH)
+    def unlock(self):
+        """Unlocks door on Relay K1."""
+        GPIO.output(self.unlock_pin, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(self.unlock_pin, GPIO.HIGH)
 
+    def is_closed(self):
+        """Return True if door is closed."""
+        return GPIO.input(self.door_switch) == GPIO.LOW
 
-def is_closed():
-    """Return True if door is closed."""
-    return GPIO.input(door_switch) == GPIO.LOW
-
-
-def is_open():
-    """Return True if door is open."""
-    return GPIO.input(door_switch) == GPIO.HIGH
+    def is_open(self):
+        """Return True if door is open."""
+        return GPIO.input(self.door_switch) == GPIO.HIGH
 
 
 def main():
+    door = Door(20, 21, 12)
+
     print("Lock")
-    lock_door()
+    door.lock_door()
     time.sleep(2)
     print("Unlock")
-    unlock_door()
+    door.unlock_door()
     time.sleep(2)
     print("Lock")
-    lock_door()
+    door.lock_door()
     time.sleep(2)
     print("Unlock")
-    unlock_door()
+    door.unlock_door()
     time.sleep(2)
 
 
