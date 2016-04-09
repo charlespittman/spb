8fa763436b9f4ad3f38fdcffe6dea0cc5af5befb
Charles Pittman <charles.pittman@gmail.com>
2016-04-09 11:14:18 -0400

    Set up objects to use.
    - Added door, gsm, rfid

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
+import Adafruit_PN532 as PN532
 import RPi.GPIO as GPIO
-import door
+import door.Door as Door
+import gsm.GSM as GSM
+import serial
 
 
 def initialize():
-    """Sets everything up.  Not sure what goes here."""
-    pass
+    """Set up the objects we'll be using later."""
 
+    door = Door(lock_pin=20, unlock_pin=21, switch_pin=12)
 
-def door_open():
-    """Returns True from door_open.py if door is open."""
-    pass
+    gsm = GSM("/dev/ttyUSB0", timeout=0.5)
+    gsm.begin()
+
+    rfid = PN532.PN532(cs=18, sclk=25, mosi=23, miso=24)
+    rfid.begin()
+    rfid.SAM_configuration()
 
 
 def mail_switch():
@@ -40,7 +46,8 @@ def intrusion_check():
 
 
 def main():
-    pass
+    initialize()
+
 
 if __name__ == '__main__':
     try:
